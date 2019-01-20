347aca5187633ce57123548091b184162624ca9c
Shrey Rastogi <shreyrastogi47.sr@gmail.com>
2019-01-20 23:20:58 +0530

    Update s5.c

--- DAA Lab/lab1/s5.c
+++ DAA Lab/lab1/s5.c
@@ -1,3 +1,14 @@
+/*
+
+LE1.5 Write a program to display an array of n integers (n>1) in O(n) time, where at every index of
+the array should contain the product of all elements in the array except the element at the
+given index. No additional array declaration is allowed.
+Example:
+Input : 10, 4, 1, 6, 2
+Output : 48,120,480,80,240
+
+*/
+
 #include<stdio.h>
 //#include<string.h>
 //#include<limits.h>
