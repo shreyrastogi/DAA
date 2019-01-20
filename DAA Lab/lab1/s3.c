79c18a5144970089a1eaf91cd89603aa2d8392e5
Shrey Rastogi <shreyrastogi47.sr@gmail.com>
2019-01-20 23:19:34 +0530

    Update s3.c

--- DAA Lab/lab1/s3.c
+++ DAA Lab/lab1/s3.c
@@ -1,3 +1,10 @@
+/*
+
+LE1.3 Given key in a sorted array A with distinct values. Write a program to find i, j, k such that
+A[i] + A[j] + A[k] == key.
+
+*/
+
 #include<stdio.h>
 //#include<string.h>
 //#include<limits.h>
